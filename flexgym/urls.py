0731f03243c7fb8296f6bf214baea9367806212a
Yousefalm1 <yousefalm@outlook.com>
2023-12-17 17:46:58 +0300

    Create new app

--- flexgym/urls.py
+++ flexgym/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from core.views import home
 
 urlpatterns = [
+    path('home/', home, name ="home"),
     path('admin/', admin.site.urls),
 ]

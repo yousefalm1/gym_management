8a8431f67189aef1015de12dd67babfab9ea13f0
Yousefalm1 <yousefalm@outlook.com>
2023-12-17 19:40:18 +0300

    Fix admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import UserProfile, InstructorProfile, MemberProfile, AdminProfile, MembershipType, GymClasses
 
-# Register your models here.
+# Register your models here
+admin.site.register(UserProfile)
+admin.site.register(InstructorProfile)
+admin.site.register(MemberProfile)
+admin.site.register(AdminProfile)
+admin.site.register(MembershipType)
+admin.site.register(GymClasses)

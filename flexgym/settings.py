98883605c48d17cea93381ad2cdc121c5390e5fc
Yousefalm1 <yousefalm@outlook.com>
2023-12-17 18:13:35 +0300

    Fix allowed host

--- flexgym/settings.py
+++ flexgym/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-qz!43&dim=&$#hal25gk0z2mccwrn=#qyw@3%v%ciw!4jzdr9#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['http://127.0.0.1:8000','https://flexgym-b1870d339c98.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition

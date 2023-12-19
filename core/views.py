a85222ff7b5443eb3485a81ec9e5ec655590f29c
Yousefalm1 <yousefalm@outlook.com>
2023-12-19 21:47:57 +0300

    Create join class function in views

--- core/views.py
+++ core/views.py
@@ -13,3 +13,13 @@ def instructors(request):
 def classes(request):
     classes = GymClasses.objects.all()
     return render(request, 'core/classes.html', {'classes': classes})
+
+def join_class(request, class_id):
+
+    gym_class = GymClasses.objects.get(pk=class_id)
+
+    if gym_class.user.count() < gym_class.max_capacity:
+        gym_class.user.add(request.user)
+        return render(request, 'core/join_success.html',{'class_name': gym_class.class_name})
+    else:
+        return render(request, 'core/join_failure.html', {'class_name': gym_class.class_name})

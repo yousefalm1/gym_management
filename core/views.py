86b22a8a6fa59a9ae6ea063599a93a546e3c2338
Yousefalm1 <yousefalm@outlook.com>
2023-12-20 20:11:39 +0300

    Add button to classes

--- core/views.py
+++ core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import GymClasses
 
@@ -17,12 +17,12 @@ def classes(request):
 
 def join_class(request, class_id):
 # Retrieve the specific gym class from the database based on the provided 'class_id'
-    gym_class = GymClasses.objects.get(pk=class_id)
+    gym_class = get_object_or_404(GymClasses, pk=class_id)
 
 # Checks if the number of users currently registered for the gym class is less than the max capacity if the class is not full the code inside will run
-    if gym_class.user.count() < gym_class.max_capacity:
+    if gym_class.users.count() < gym_class.max_capacity:
         # Adds the current user to the gym class by updating the 'users' field which is a ManyToManyField
-        gym_class.user.add(request.user)
+        gym_class.users.add(request.user)
         # After successfully adding the user to the class the view renders the html and passes the class name 
         return render(request, 'core/join_success.html',{'class_name': gym_class.class_name})
     # If the class is full the code inside the block is executed and renders the join_failure.html template and passes the class name

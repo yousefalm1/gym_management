49a6c0d8ee343ba6d5303db3b5b1cc7982e8ffdd
Yousefalm1 <yousefalm@outlook.com>
2023-12-21 19:17:04 +0300

    Add the allauth template files to the projects templates

--- flexgym/urls.py
+++ flexgym/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from core.views import index, instructors, classes, join_class
 
 urlpatterns = [
+    path("accounts/", include("allauth.urls")),
     path('instructors/', instructors, name='instructors'),
     path('classes/', classes, name='classes'),
     path('join_class/<int:class_id>/', join_class, name='join_class'),

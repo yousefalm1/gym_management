fa970318f32d33322cc115baa5f9fd35f7f098d4
Yousefalm1 <yousefalm@outlook.com>
2023-12-22 17:16:12 +0300

    Move templates folder outsdie the app so the login ans sign up template can work

--- core/views.py
+++ core/views.py
@@ -3,16 +3,16 @@ from django.http import HttpResponse
 from .models import GymClasses
 
 def index(request):
-    return render(request, 'core/index.html')
+    return render(request, 'index.html')
 
 
 def instructors(request):
-    return render(request, 'core/instructors.html')
+    return render(request, 'instructors.html')
 
 
 def classes(request):
     classes = GymClasses.objects.all()
-    return render(request, 'core/classes.html', {'classes': classes})
+    return render(request, 'classes.html', {'classes': classes})
 
 
 def join_class(request, class_id):
--- flexgym/urls.py
+++ flexgym/urls.py
@@ -3,10 +3,10 @@ from django.urls import path,include
 from core.views import index, instructors, classes, join_class
 
 urlpatterns = [
-    path("accounts/", include("allauth.urls")),
     path('instructors/', instructors, name='instructors'),
     path('classes/', classes, name='classes'),
     path('join_class/<int:class_id>/', join_class, name='join_class'),
+    path("accounts/", include("allauth.urls")),
     path('admin/', admin.site.urls),
     path('', index, name='index'),
 

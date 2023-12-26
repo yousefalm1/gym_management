83695759fdc0727cc072550aca3b0534e12babc8
Yousefalm1 <yousefalm@outlook.com>
2023-12-26 19:57:35 +0300

    Fix button so if user has no memberhsip they cant click and shows they dont have a memberhsip

--- core/views.py
+++ core/views.py
@@ -31,14 +31,20 @@ def join_class(request, class_id):
 # Retrieve the specific gym class from the database based on the provided 'class_id'
     gym_class = get_object_or_404(GymClasses, pk=class_id)
 
-# Checks if the number of users currently registered for the gym class is less than the max capacity if the class is not full the code inside will run
-    if gym_class.users.count() < gym_class.max_capacity:
-        # Adds the current user to the gym class by updating the 'users' field which is a ManyToManyField
-        gym_class.users.add(request.user)
-        # After successfully adding the user to the class the view renders the html and passes the class name 
-        return render(request, 'join_success.html',{'class_name': gym_class.class_name})
-    # If the class is full the code inside the block is executed and renders the join_failure.html template and passes the class name
-    else:
-        return render(request, 'join_failure.html', {'class_name': gym_class.class_name})
+
+
+    # Check if the user has a membership
+    user_profile, created = UserProfile.objects.get_or_create(user=request.user)
+    if user_profile.membership_choices:
+    # Checks if the number of users currently registered for the gym class is less than the max capacity if the class is not full the code inside will run
+        if gym_class.users.count() < gym_class.max_capacity:
+            # Adds the current user to the gym class by updating the 'users' field which is a ManyToManyField
+            gym_class.users.add(request.user)
+            # After successfully adding the user to the class the view renders the html and passes the class name 
+            return render(request, 'join_success.html',{'class_name': gym_class.class_name})
+        # If the class is full the code inside the block is executed and renders the join_failure.html template and passes the class name
+        else:
+            return render(request, 'join_failure.html', {'class_name': gym_class.class_name})
+        
     
 

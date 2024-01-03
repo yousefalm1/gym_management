7dd887c2198338c41e7e3285b8ae3a51bb3f464e
Yousefalm1 <yousefalm@outlook.com>
2024-01-03 13:16:08 +0300

    Add create user profile form to forms.py

--- core/forms.py
+++ core/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import InstructorProfile, GymClasses
+from .models import InstructorProfile, GymClasses, UserProfile
 
 class InstructorProfileForm(forms.ModelForm):
     #  provide metadata about the form so for this case it is about the model InstructorProfile so the form will be based off the fields in the model
@@ -13,3 +13,9 @@ class GymClassForm(forms.ModelForm):
     class Meta:
         model = GymClasses
         fields = ['class_name', 'room_number', 'description', 'max_capacity', 'class_image', 'instructors']
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        Model = UserProfile
+        fields = ['user', 'membership_choices', 'join_date', 'new_membership_purchase']

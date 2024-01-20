a00770414c202ff5b873d8212a7f8462b41f38e6
Yousefalm1 <yousefalm@outlook.com>
2024-01-20 18:30:31 +0300

    Fix base.html

--- flexgym/settings.py
+++ flexgym/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = [
 ]
 # settings.py
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
 
 
 # Default primary key field type

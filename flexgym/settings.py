d938709e471ee8e8816aba310f16c9a85af85562
Yousef Almesaeed <yousefalm@Outlook.com>
2024-06-21 17:48:40 +0300

    fix Allowed hosts

--- flexgym/settings.py
+++ flexgym/settings.py
@@ -17,8 +17,8 @@ SECRET_KEY = 'django-insecure-qz!43&dim=&$#hal25gk0z2mccwrn=#qyw@3%v%ciw!4jzdr9#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1',
-                 'https://gym-management16-380f4abf25ba.herokuapp.com/']
+ALLOWED_HOSTS = os.environ.get(
+    'ALLOWED_HOSTS', 'https://gym-management16-380f4abf25ba.herokuapp.com/').split(',')
 
 # Application definition
 
